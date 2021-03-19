2f0f58e74354f770e7cf5c6c324fe129ba0766fb
kitami12 <toranekotoaisu@gmail.com>
2021-03-19 22:06:23 +0900

    viewの変更　user_idがあるテーブルの値を画面に表示

--- src/Controller/SkillSheetsController.php
+++ src/Controller/SkillSheetsController.php
@@ -1,53 +1,59 @@
 <?php
-// src/Controller/SkillsController.php
+    // src/Controller/SkillsController.php
 
-namespace App\Controller;
-use Cake\ORM\TableRegistry;
-class SkillSheetsController extends AppController
-{
-    public function initialize() {
-        parent::initialize();
-
-        $this->loadComponent('paginator');
-        $this->loadComponent('Flash');
-        $this->Auth->allow(['logout','add']);
-        // ログインしなくてもlogoutとaddは表示できる
-        $this->Users = TableRegistry::getTableLocator()->get('Users');
-    }
-    public function index()
+    namespace App\Controller;
+    use Cake\ORM\TableRegistry;
+    class SkillSheetsController extends AppController
     {
-        parent::initialize();
-    }
+            public function initialize() {
+                parent::initialize();
 
-    
-    public function view($id = null)
-    {
-        $users = $this->Users->get($id, [
-            'contain' => ['Profiles','Programmings','Osses','Softs','Langs',
-            'Qualifications','Performances','Frees','Environs','Businesses'],
-        ]);
+                $this->loadComponent('paginator');
+                $this->loadComponent('Flash');
+                $this->Auth->allow(['logout','add']);
+                // ログインしなくてもlogoutとaddは表示できる
+                $this->Users = TableRegistry::getTableLocator()->get('Users');
+                $this->Profiles = TableRegistry::getTableLocator()->get('Profiles');
+                $this->Programmings = TableRegistry::getTableLocator()->get('Programmings');
+                $this->Osses = TableRegistry::getTableLocator()->get('Osses');
+                $this->Softs = TableRegistry::getTableLocator()->get('Softs');
+                $this->Langs = TableRegistry::getTableLocator()->get('Langs');
+                $this->Qualifications = TableRegistry::getTableLocator()->get('Qualifications');
+                $this->Performances = TableRegistry::getTableLocator()->get('Performances');
+                $this->Frees = TableRegistry::getTableLocator()->get('Frees');
+                $this->Environs = TableRegistry::getTableLocator()->get('Environs');
+                $this->Businesses = TableRegistry::getTableLocator()->get('Businesses');
+                // $this->Abilities = TableRegistry::getTableLocator()->get('Abilities');
+            } 
+            public function index() {
+                parent::initialize();
+            }
 
-        $this->set(compact('users'));
         
-    }
+            public function view($id = null) {
 
-    public function add()
-    {
-        
-        
-        
-    }
+                $user = $this->Users->get($id, [
+                    'contain' => ['Profiles','Programmings','Osses','Softs','Langs',
+                    'Qualifications','Performances','Frees','Environs','Businesses'],
+                ]);
 
-  
+                $this->set(compact('user'));
+            
+            }
+
+            public function add() {
+            }
+
+            
 
 
-    public function isAuthorized($user)
-{
-    $action = $this->request->getParam('action');
-    // add および tags アクションは、常にログインしているユーザーに許可されます。
-    if (in_array($action, ['add', 'view'])) {
-        return true;
+                public function isAuthorized($user) {
+                    
+                    $action = $this->request->getParam('action');
+                    //edit アクションは、常にログインしているユーザーに許可されます。
+                    if (in_array($action, ['edit','add'])) {
+                        return true;
+                    }
+                    return $user->user_id === $user['id'];
+                }
     }
-    return $user->user_id === $user['id'];
-    }
-}
